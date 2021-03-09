5a3c5697e7e8c44ce3abbd14548fd0ffb14ad41d
Morten Gross <mg@vuffeli.dk>
2021-03-09 09:34:31 +0100

    The text is read and replaced in JS. Animation still working

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("init");
+    let coolText = document.querySelector("#cooltext");
+    const contentText = coolText.textContent;
+    const splitText = contentText.split('');
+    
+    coolText.textContent = "";
+     
+    splitText.forEach(letter => {
+        const span = document.createElement("span");
+        span.classList.add("fade_in");
+    
+        if (letter === " ") {
+            span.innerHTML = "&nbsp;";
+        } else {
+            span.textContent = letter;
+        }
+        console.log(span);
+        coolText.append(span); 
+    });
 }
