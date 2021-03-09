e574ce541e9984a0b6ac5b0c504104f0f31a5b48
Morten Gross <mg@vuffeli.dk>
2021-03-09 10:07:51 +0100

    console.logs removed. Commenting added

--- script.js
+++ script.js
@@ -3,25 +3,32 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log("init");
+    // Get the text, and split it into an array
     let coolText = document.querySelector("#cooltext");
     const contentText = coolText.textContent;
     const splitText = contentText.split('');
     
+    // Empty the HTML tag containing the text
     coolText.textContent = "";
      
     splitText.forEach((letter, index) => {
+        // Create a span element for each letter, and add the animation
         const span = document.createElement("span");
         span.classList.add("fade_in");
+
+        // Set the animation delay to the index of the span
         span.style.setProperty("--delay", index);
     
+        // Makes sure that the spaces doesn't collapse 
         if (letter === " ") {
             span.innerHTML = "&nbsp;";
-        } else {
+        } 
+        // Otherwise sets the content of the spans to the letters
+        else {
             span.textContent = letter;
         }
 
-        console.log(span);
+        // Add the spans to the HTML tag
         coolText.append(span); 
     });
 }
